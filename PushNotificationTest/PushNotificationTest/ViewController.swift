7448817d667c0493305fd214ef755782b95799fc
Wayne <xhhuango@gmail.com>
2020-05-09 22:29:16 +0800

    Add PushNotificationTest

---
+++ PushNotificationTest/PushNotificationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushNotificationTest
+//
+//  Created by Wayne on 2020/5/8.
+//  Copyright © 2020 Wayne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
